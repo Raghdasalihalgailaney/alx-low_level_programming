f24fb6d8dfecb3211b17186c86b7c277170c2d45
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-09-13 07:25:24 -0700

    My new file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,20 @@
 */
 void print_alphabet_x10(void)
 {
-char ch;
-int i;
-i = 0;
-while (i < 10)
+	char ch;
+	int i;
+
+	i = 0;
+	while (i < 10)
 {
-ch = 'a';
-while (ch <= 'z')
+	ch = 'a';
+	while (ch <= 'z')
 {
-_putchar(ch);
-ch++;
+	_putchar(ch);
+	ch++;
 }
-_putchar('\n');
-i++;
+	_putchar('\n');
+	i++;
+}
+
 }

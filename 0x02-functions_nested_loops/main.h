a13272bc1a1ee9a1ab7b631d766397383195360e
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-09-12 18:48:51 -0700

    My files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef main_h 
-#define main_h
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
-#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);

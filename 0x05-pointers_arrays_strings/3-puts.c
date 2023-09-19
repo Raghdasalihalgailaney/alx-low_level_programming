90aefa670b757d04da0776dc37e987e613d4902c
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-09-19 10:53:57 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,25 +1,13 @@
 #include "main.h"
 /**
- *_puts - prints a string
- *@str: string to print
- *
- *Description: prints a string
- *On Success: return the number of characters printed
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-
-/*
-1. Declare a function called _puts that takes a pointer to a char as a parameter.
-2. Create a while loop that will iterate through the string until it reaches the null byte.
-3. Inside the while loop, call the _putchar function to print the current character.
-4. After the while loop, print a new line.
-*/
-/*
-Time Complexity: O(n)
-*/
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
-
+	while (*str != '\0')
+	{
+	_putchar(*str++);
+	}
 	_putchar('\n');
 }

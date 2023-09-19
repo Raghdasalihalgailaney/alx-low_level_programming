23bf27d20e42a271a83d43e43ebda5dc63e3f85c
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-09-19 09:46:50 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,25 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _puts - check the code for Holberton School students.
- * @str: validate the character
- * Return: Always 0.
+ *_puts - prints a string
+ *@str: string to print
+ *
+ *Description: prints a string
+ *On Success: return the number of characters printed
  */
+
+/*
+1. Declare a function called _puts that takes a pointer to a char as a parameter.
+2. Create a while loop that will iterate through the string until it reaches the null byte.
+3. Inside the while loop, call the _putchar function to print the current character.
+4. After the while loop, print a new line.
+*/
+/*
+Time Complexity: O(n)
+*/
 void _puts(char *str)
 {
-	int i;
+	while (*str)
+		_putchar(*str++);
 
-	for (i = 0; str[i] != '\0' ; i++)
-	{
-		_putchar(str[i]);
-	}
 	_putchar('\n');
 }

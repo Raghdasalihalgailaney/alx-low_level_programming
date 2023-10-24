60c57175a496ca951f8bbb31139bd33986d18469
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-10-24 08:09:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,16 +2,18 @@
 /**
  * print_listint - prints alinked listis
  * @h: pointer to first node
+ *
  * Return: size of list
  */
 size_t print_listint(const listint_t *h);
 {
-	size_t i = 0;
+	size_t num = 0;
+
 	while (h)
 	{
 	printf("%d\n", h->n);
 	h = h->next;
-	i++;
+	num++;
 	}
-	return (i);
+	return (num);
 }

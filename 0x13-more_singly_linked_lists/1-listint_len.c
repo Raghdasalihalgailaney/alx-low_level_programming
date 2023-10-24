bac3073794108c77f3653f1b052729c7b163aee5
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-10-23 19:26:59 -0700

    My jobe

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * print_listint - prints alinked listis
+ * @h: pointer to first node
+ * Return: size of list
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_t i = 0;
+	while (h)
+	{
+	h = h->next;
+	i++;
+	}
+	return (i);
+}

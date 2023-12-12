34f164981a009ecf473a949216e547994e6b7633
Raghdasalihalgailaney <roodysalih299@gmail.com>
2023-12-12 09:52:59 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * print_listint - prints alinked listis
+ * print_listint - prints alinked lists
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 {
 	size_t num = 0;
 
